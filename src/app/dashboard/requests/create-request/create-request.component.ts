35489bd2218969fda2cc9e0fcd9b58260d0f807a
= <=>
2024-06-06 20:25:34 +0000

    feat: implement beneficiary requests and create requests

---
+++ src/app/dashboard/requests/create-request/create-request.component.ts
@@ -0,0 +1,182 @@
+import { Component, OnDestroy } from '@angular/core';
+import { SpinnerComponent } from '@app/shared/spinner/spinner.component';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButton, MatIconButton } from '@angular/material/button';
+import { InfoCardComponent } from '@app/shared/info-card/info-card.component';
+import {
+  FormArray,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import {
+  MatFormField,
+  MatPrefix,
+  MatSuffix,
+} from '@angular/material/form-field';
+import { MatSelect } from '@angular/material/select';
+import { MatInput, MatLabel } from '@angular/material/input';
+import { BannerComponent } from '@app/shared/banner/banner.component';
+import { catchError, finalize, first, of, Subject } from 'rxjs';
+import { Location } from '@angular/common';
+import { MatDialog, MatDialogClose } from '@angular/material/dialog';
+import {
+  CreateDisbursement,
+  CreateDisbursementDistribution,
+} from '@app/dashboard/finance/disbursement/disbursement.interface';
+import { serverError } from '@app/libs/constants';
+import {
+  ActionModalData,
+  ActionModalIllustration,
+} from '@app/shared/action-modal/action-modal.type';
+import { ActionModalComponent } from '@app/shared/action-modal/action-modal.component';
+import { FinanceService } from '@app/dashboard/finance/finance.service';
+import { ToastrService } from 'ngx-toastr';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-create-request',
+  standalone: true,
+  imports: [
+    SpinnerComponent,
+    MatIconModule,
+    MatButton,
+    InfoCardComponent,
+    ReactiveFormsModule,
+    MatFormField,
+    MatSelect,
+    MatInput,
+    MatPrefix,
+    MatSuffix,
+    BannerComponent,
+    MatDialogClose,
+    MatIconButton,
+    MatLabel,
+  ],
+  templateUrl: './create-request.component.html',
+  styleUrl: './create-request.component.scss',
+})
+export class CreateRequestComponent implements OnDestroy {
+  isLoading = false;
+  showBanner = false;
+  bannerText = '';
+  requestForm = this.fb.group({
+    amount: ['', Validators.required],
+    disbursementDistribution: this.fb.array([]),
+  });
+  public distributionForm = this.fb.group({
+    title: ['', Validators.required],
+    amount: ['', Validators.required],
+  });
+  protected _onDestroy = new Subject<void>();
+  public totalDistribution = 0;
+
+  constructor(
+    private readonly fb: FormBuilder,
+    private readonly location: Location,
+    private readonly financeService: FinanceService,
+    private readonly toastrService: ToastrService,
+    private readonly dialog: MatDialog,
+    private readonly router: Router
+  ) {}
+
+  ngOnDestroy() {
+    this._onDestroy.next();
+    this._onDestroy.complete();
+  }
+
+  get disbursementDistributions(): FormArray {
+    return this.requestForm.controls.disbursementDistribution as FormArray;
+  }
+
+  addDistribution() {
+    if (this.requestForm.controls.amount.valid) {
+      const amount = this.distributionForm.controls.amount.value;
+      const values = new FormGroup({
+        title: new FormControl(this.distributionForm.controls.title.value),
+        amount: new FormControl(amount),
+      });
+      this.totalDistribution += parseInt(amount as string);
+      const totalBudget = parseInt(
+        this.requestForm.controls.amount.value || '0'
+      );
+      if (this.totalDistribution > totalBudget) {
+        this.bannerText =
+          'Your disbursement distribution as exceeded your total budget.';
+        this.showBanner = true;
+        return;
+      }
+      this.disbursementDistributions.push(values);
+      this.distributionForm.reset();
+    } else {
+      this.bannerText = 'Please enter disbursement amount before distribution';
+      this.showBanner = true;
+    }
+  }
+
+  deleteDistribution(index: number) {
+    this.totalDistribution -= parseInt(
+      this.disbursementDistributions.at(index).value
+    );
+    this.disbursementDistributions.removeAt(index);
+  }
+
+  closeBanner() {
+    this.showBanner = !this.showBanner;
+  }
+
+  cancel() {
+    this.location.back();
+  }
+
+  submit() {
+    if (this.disbursementDistributions.length) {
+      const disbursementDetails: Omit<CreateDisbursement, 'studentId'> = {
+        amount: parseInt(this.requestForm.value.amount as string),
+        disbursementDistribution: this.requestForm.controls
+          .disbursementDistribution.value as CreateDisbursementDistribution[],
+      };
+      this.isLoading = true;
+      this.financeService
+        .createBeneficiaryDisbursement(disbursementDetails)
+        .pipe(
+          first(),
+          catchError(error => {
+            this.toastrService.error(
+              error.error.message,
+              error.error.error || serverError
+            );
+            return of(null);
+          }),
+          finalize(() => {
+            this.isLoading = false;
+          })
+        )
+        .subscribe(response => {
+          if (response) {
+            const data: ActionModalData = {
+              actionIllustration: ActionModalIllustration.success,
+              title: 'Congratulations!',
+              actionColor: 'primary',
+              subtext: 'Well done, You have successfully created a request',
+              actionType: 'close',
+            };
+            this.dialog.open(ActionModalComponent, {
+              maxWidth: '400px',
+              maxHeight: '400px',
+              width: '100%',
+              height: '100%',
+              data,
+            });
+            void this.router.navigate(['dashboard/requests']);
+          }
+        });
+    } else {
+      this.bannerText =
+        'Please make sure your distribution equals disbursement amount';
+      this.showBanner = true;
+    }
+  }
+}
---
+++ src/app/dashboard/requests/requests.component.ts
@@ -0,0 +1,101 @@
+import { AfterViewInit, Component } from '@angular/core';
+import { CurrencyPipe, DatePipe, TitleCasePipe } from '@angular/common';
+import { RoundedInputComponent } from '@app/shared/rounded-input/rounded-input.component';
+import { MatMenuModule } from '@angular/material/menu';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatRadioButton, MatRadioGroup } from '@angular/material/radio';
+import {
+  combineLatest,
+  debounceTime,
+  of as observableOf,
+  Subject,
+  takeUntil,
+} from 'rxjs';
+import { SpinnerComponent } from '@app/shared/spinner/spinner.component';
+import { Disbursement } from '@app/dashboard/finance/disbursement/disbursement.interface';
+import { MatTableModule } from '@angular/material/table';
+import { MatSort } from '@angular/material/sort';
+import { MatChip } from '@angular/material/chips';
+import { MatIcon } from '@angular/material/icon';
+import { MatPaginator, PageEvent } from '@angular/material/paginator';
+import { RouterLink } from '@angular/router';
+import { catchError, map, startWith, switchMap } from 'rxjs/operators';
+import { FinanceService } from '@app/dashboard/finance/finance.service';
+
+@Component({
+  selector: 'app-requests',
+  standalone: true,
+  imports: [
+    CurrencyPipe,
+    RoundedInputComponent,
+    MatMenuModule,
+    MatButtonModule,
+    MatRadioGroup,
+    MatRadioButton,
+    ReactiveFormsModule,
+    SpinnerComponent,
+    MatTableModule,
+    MatSort,
+    MatChip,
+    TitleCasePipe,
+    MatIcon,
+    MatPaginator,
+    DatePipe,
+    RouterLink,
+  ],
+  templateUrl: './requests.component.html',
+  styleUrl: './requests.component.scss',
+})
+export class RequestsComponent implements AfterViewInit {
+  public readonly displayedColumns: string[] = [
+    'created_at',
+    'amount',
+    'status',
+    'more',
+  ];
+  public searchValue = new FormControl('');
+  public categoryControl = new FormControl('');
+  public categoryFilters = [
+    { value: 'Professional Course', label: 'Professional Course' },
+  ];
+  public isLoadingResults = false;
+  public totalItems = 0;
+  public page = new FormControl(1);
+  private readonly destroy = new Subject<void>();
+  public statusFilters = [{ value: 'pending', label: 'Pending' }];
+  public statusControl = new FormControl('');
+  public data: Disbursement[] = [];
+
+  constructor(private readonly financeService: FinanceService) {}
+
+  ngAfterViewInit() {
+    combineLatest([
+      this.page.valueChanges.pipe(startWith(1)),
+      this.statusControl.valueChanges.pipe(startWith('')),
+    ])
+      .pipe(
+        takeUntil(this.destroy),
+        debounceTime(1000),
+        switchMap(([page, status]) => {
+          this.isLoadingResults = true;
+          return this.financeService
+            .getBeneficiaryDisbursements(page || 1, status || '')
+            .pipe(catchError(() => observableOf(null)));
+        }),
+        map(data => {
+          this.isLoadingResults = false;
+          if (data === null) {
+            return [];
+          }
+          this.totalItems = data.data.total;
+          return data.data.items;
+        })
+      )
+      .subscribe(data => (this.data = data));
+  }
+
+  onPaginationChange(event: PageEvent): void {
+    this.page.setValue(event.pageIndex + 1);
+  }
+}
